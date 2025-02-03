dd9251de7875bccc9df57f6c15c46c818867ef6a
jhocoder <jhonathanrugeb@gmail.com>
2025-02-03 13:19:57 +0100

    create with requeriment add routes

---
+++ src/app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from dotenv import load_dotenv
+import os
+
+app = Flask(__name__)
+
+load_dotenv()
+
+
+@app.route('/')
+def index():
+    username = os.getenv('USER')
+    email = os.getenv('EMAIL')
+    password = os.getenv('PASSWORD')
+    
+    print(username, email, password)
+    
+    
+    return '<h1>app flask desployeada en render.<br> {}<br> {} <br> {}</h1>'.format(username, email, password)
+
+
+def status_404(error):
+    return '<h1> Pagina no encontrada </h1>'
+
+if __name__ == "__main__":
+    app.register_error_handler(404, status_404)
+    app.run(debug=True, port=8080, host="localhost")
